95b92924cb2144937b4e480e6423ea67daa09404
JaswantSharma <droidude1@gmail.com>
2022-11-01 14:16:17 +0530

    added getvalue and set value in customer from desktop

--- gitpractice/customer.h
+++ gitpractice/customer.h
@@ -11,4 +11,8 @@ public:
 	Customer(); // Defualt constructor
 	Customer(string username);// Parameterized Constructor
 
+	//these are the helping functions for get the values from the file
+	string getvalue(char* filename, string user);
+	void setvalue(char* filename, char* tempfile, string proid, string updatevalue);
+
 };
